365333bc1393871c9090fc13a8aa8f4cccaa59e8
Kate Brown <katebrown2901@gmail.com>
2024-02-24 16:49:28 +0000

    Refactored to use reflection

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Buffers;
+using System.Security.Cryptography.X509Certificates;
 using FizzBuzz.Rules;
 
 namespace FizzBuzz
@@ -12,14 +13,21 @@ namespace FizzBuzz
             //Generate number range. 
             var numbers = new Utils.NumberGenerator().GetNumberList(1, 100);
 
-            //Generate rules.
-            var fizzRule = new FizzRule();
-            var buzzRule = new BuzzRule();
+            //TODO - Put rule generation into a separate class.
+            //Find all classes implmenting the IRule interface, then instantiate them. 
+            List<System.Type> ruleClasses = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes())
+            .Where(x => typeof(Rules.IRule).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+            .Select(x => x).ToList();
 
-            //Add rules to rule list.
             var rules = new List<Rules.IRule>();
-            rules.Add(fizzRule);
-            rules.Add(buzzRule);
+
+            foreach (var ruleClass in ruleClasses)
+            {
+                var rule = (Rules.IRule)Activator.CreateInstance(ruleClass);
+                rules.Add(rule);
+            } 
+            //Without this you get 'BuzzFizz' for the combined rule... 
+            rules = rules.OrderBy(x => x.divisor).ToList();
 
             //Generate the FizzBuzzer, pass in the rules and the number range and print! 
             var numberParser = new NumberParser(rules, numbers);
